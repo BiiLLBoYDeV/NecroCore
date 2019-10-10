748e3c95a960ca622e4266e61e78891437f2966e
Necromancers <blank_@outlook.fr>
2019-10-10 19:13:01 +0200

    Ajout des modules

---
+++ src/server/game/LuaEngine/modules/modules_active.lua
@@ -0,0 +1,26 @@
+	--[[##########################################
+	#
+	# - Script created by Necromancers
+	# - Script for NecroCore
+	#
+	# Support me : paypal.me/Necromancers
+	#
+	##########################################]]--
+
+	Script {
+		--[[ COMMANDS ]]--
+			-- Admin Create Module ( Vidéo : ) --
+			Mod_AdminCreateModule = 0,
+			-- Event Creator (Vidéo : ) --
+			Mod_EventCreator = 0,
+
+		--[[ SYSTEM ]]--
+			-- Scaling Level System ( Vidéo : ) --
+			Mod_ScalingLevelSystem = 0,
+
+		--[[ GAMEPLAY ]]--
+			-- Congrats at LevelUp (Vidéo : ) --
+			Mod_CongratsAtLevelup = 0,
+			-- Modifier Exp Rates (Vidéo : ) --
+			Mod_RatesExp = 0;
+	};
